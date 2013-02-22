af49adb59e910a716ce40b4c1c948c592017c8e7
Roger Allen <rallen@gmail.com>
2013-02-22 07:36:34 -0800

    found better way to get to 60Hz update.  snapshot beta code

--- src/hello_lwjgl/core.clj
+++ src/hello_lwjgl/core.clj
@@ -5,14 +5,13 @@
 
 ;; ======================================================================
 ;; example alpha:
-;; behold, a simple, old-fashioned spinning triangle
+;; behold, a simple, old-fashioned (OpenGL 1.1) spinning triangle
 (defn alpha-init-window
   [width height title]
   (def alpha-globals (ref {:width width
                            :height height
                            :title title
                            :angle 0.0
-                           :next-time (System/currentTimeMillis)
                            :last-time (System/currentTimeMillis)}))
   (Display/setDisplayMode (DisplayMode. width height))
   (Display/setTitle title)
@@ -51,29 +50,18 @@
 
 (defn alpha-update
   []
-  (let [{:keys [width height angle next-time last-time]} @alpha-globals
+  (let [{:keys [width height angle last-time]} @alpha-globals
         cur-time (System/currentTimeMillis)
         delta-time (- cur-time last-time)
-        sleep-time (max 4 (/ (- next-time cur-time) 2))
-        do-draw (> cur-time next-time)
         next-angle (+ (* delta-time 0.05) angle)
         next-angle (if (>= next-angle 360.0)
                      (- next-angle 360.0)
-                     next-angle)
-        next-time (if do-draw
-                    (+ 8 cur-time)
-                    next-time)]
-
+                     next-angle)]
     (dosync (ref-set alpha-globals
                      (assoc @alpha-globals
                        :angle next-angle
-                       :next-time next-time
                        :last-time cur-time)))
-    ;; not exactly sure this is the best way to control refresh rate
-    ;; but it seems to work for me
-    (if do-draw
-      (alpha-draw)
-      (Thread/sleep sleep-time))))
+    (alpha-draw)))
 
 (defn alpha-run
   []
@@ -81,6 +69,7 @@
   (alpha-init-gl)
   (while (not (Display/isCloseRequested))
     (alpha-update)
+    (Display/sync 60)
     (Display/update))
   (Display/destroy))
 
@@ -89,37 +78,76 @@
   (.start (Thread. alpha-run)))
 
 ;; ======================================================================
-;; beta - TODO
-;;   do the same thing in OpenGL 3.2
+;; beta 
+;;   same spinning triangle in OpenGL 3.2
 (defn beta-init-window
   [width height title]
   (let [pixel-format (PixelFormat.)
         context-attributes (-> (ContextAttribs. 3 2)
                                (.withForwardCompatible true)
-                               (.withProfileCore true))]
+                               (.withProfileCore true))
+        current-time-millis (System/currentTimeMillis)]
   (def beta-globals (ref {:width width
-                           :height height
-                           :title title
-                           :angle 0.0
-                           :next-time (System/currentTimeMillis)
-                           :last-time (System/currentTimeMillis)}))
+                          :height height
+                          :title title
+                          :angle 0.0
+                          :last-time current-time-millis}))
   (Display/setDisplayMode (DisplayMode. width height))
   (Display/setTitle title)
-  (println pixel-format context-attributes)
   (Display/create pixel-format context-attributes)))
 
 (defn beta-init-gl
   []
   (println "OpenGL version:" (GL11/glGetString GL11/GL_VERSION)))
 
+(defn beta-draw
+  []
+  (let [{:keys [width height angle]} @beta-globals
+        w2 (/ width 2.0)
+        h2 (/ height 2.0)]
+    (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
+
+    (comment "FIXME!"
+             (GL11/glLoadIdentity)
+             (GL11/glTranslatef w2 h2 0)
+             (GL11/glRotatef angle 0 0 1)
+             (GL11/glScalef 2 2 1)
+             (GL11/glBegin GL11/GL_TRIANGLES)
+             (do
+               (GL11/glColor3f 1.0 0.0 0.0)
+               (GL11/glVertex2i 100 0)
+               (GL11/glColor3f 0.0 1.0 0.0)
+               (GL11/glVertex2i -50 86.6)
+               (GL11/glColor3f 0.0 0.0 1.0)      
+               (GL11/glVertex2i -50 -86.6)
+               )
+             (GL11/glEnd))
+    ))
+
+(defn beta-update
+  []
+  (let [{:keys [width height angle last-time]} @beta-globals
+        cur-time (System/currentTimeMillis)
+        delta-time (- cur-time last-time)
+        next-angle (+ (* delta-time 0.05) angle)
+        next-angle (if (>= next-angle 360.0)
+                     (- next-angle 360.0)
+                     next-angle)]
+    (dosync (ref-set beta-globals
+                     (assoc @beta-globals
+                       :angle next-angle
+                       :last-time cur-time)))
+    (beta-draw)))
+
 (defn beta-run
   []
   (beta-init-window 800 600 "beta")
   (beta-init-gl)
   (Thread/sleep 2000)
-  ;;(while (not (Display/isCloseRequested))
-  ;;  (beta-update)
-  ;;  (Display/update))
+  (while (not (Display/isCloseRequested))
+    (beta-update)
+    (Display/sync 60)
+    (Display/update))
   (Display/destroy))
 
 (defn beta-main []
