13deb42a50ffe358a989db77935752b776aa874b
Roger Allen <rallen@gmail.com>
2013-02-22 16:46:57 -0800

    cleanup imports

--- src/hello_lwjgl/core.clj
+++ src/hello_lwjgl/core.clj
@@ -1,10 +1,6 @@
 (ns hello-lwjgl.core
   (:require [hello-lwjgl.alpha :as alpha]
             [hello-lwjgl.beta :as beta])
-  (:import (java.nio ByteBuffer FloatBuffer)
-           (org.lwjgl BufferUtils)
-           (org.lwjgl.opengl ContextAttribs Display DisplayMode GL11 GL15 GL20 GL30 PixelFormat)
-           (org.lwjgl.util.glu GLU))
   (:gen-class))
 
 ;; ======================================================================
@@ -15,6 +11,4 @@
   (cond
    (= "alpha" (first args)) (alpha/main)
    (= "beta" (first args)) (beta/main)
-   true (alpha/main)))
-  ;;(-main ["alpha"])
-  ;;@alpha-globals
+   true (alpha/main))) ;; run alpha by default
