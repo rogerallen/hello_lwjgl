7ce6dec2883d0937d0cf404813afd39225147fa0
Roger Allen <rallen@gmail.com>
2014-01-13 07:40:35 -0800

    INTEL works!!!  Snapshot this before I make any more changes.

--- src/hello_lwjgl/delta.clj
+++ src/hello_lwjgl/delta.clj
@@ -1,9 +1,33 @@
 (ns hello-lwjgl.delta
   (:require [clojure.pprint :as pprint])
   (:import (java.nio ByteBuffer FloatBuffer)
+           (java.lang.reflect Field)
            (org.lwjgl BufferUtils)
            (org.lwjgl.opengl ContextAttribs Display DisplayMode GL11 GL15 GL20 PixelFormat)
            (org.lwjgl.util.glu GLU)))
+
+;; ======================================================================
+;; some code cribbed from
+;; https://github.com/ztellman/penumbra/blob/master/src/penumbra/opengl/core.clj
+;; Causes errors in glGetError to throw an exception
+(def containers [GL11 GL15 GL20])
+(defn- get-fields [#^Class static-class]
+  (. static-class getFields))
+(defn- enum-name
+  "Takes the numeric value of a gl constant (i.e. GL_LINEAR), and gives the name"
+  [enum-value]
+  (if (= 0 enum-value)
+    "NONE"
+    (.getName
+     #^Field (some
+              #(if (= enum-value (.get #^Field % nil)) % nil)
+              (mapcat get-fields containers)))))
+(defn- except-gl-errors
+  [msg]
+  (let [error (GL11/glGetError)
+        error-string (str "OpenGL Error(" error "):" (enum-name error) ": " msg)]
+    (if (not (zero? error))
+      (throw (Exception. error-string)))))
 
 ;; ======================================================================
 ;; spinning triangle in OpenGL 2.1
@@ -20,12 +44,13 @@
                        :last-time current-time-millis
                        ;; geom ids
                        :vbo-id 0
-                       :indices-count 0
+                       :vertices-count 0
+                       :cbo-id 0
                        ;; shader program ids
                        :vs-id 0
                        :fs-id 0
                        :p-id 0
-                       ::angle-loc 0}))
+                       :angle-loc 0}))
     (Display/setDisplayMode (DisplayMode. width height))
     (Display/setTitle title)
     (Display/setVSyncEnabled true)
@@ -36,27 +61,49 @@
   []
   ;; FIXME – DRY!
   (let [vertices (float-array
-                  [ 0.00  0.00 0.0 1.0
-                   -0.50 -0.50 0.0 1.0
-                    0.50 -0.50 0.0 1.0])
+                  [ 0.0  0.0  -0.5
+                    0.5 -0.5  -0.5
+                   -0.5 -0.5  -0.5
+
+                    0.0  0.0  -0.5
+                   -0.5  0.5  -0.5
+                    0.5  0.5  -0.5
+                   ])
         vertices-count (count vertices)
         vertices-buffer (-> (BufferUtils/createFloatBuffer vertices-count)
                             (.put vertices)
+                            (.flip))
+        colors (float-array
+                  [1.0  1.0  1.0
+                   1.0  1.0  0.0
+                   1.0  1.0  0.0
+
+                   1.0  1.0  1.0
+                   1.0  1.0  0.0
+                   1.0  1.0  0.0])
+        colors-count (count vertices) ;; better be the same as vertices-count
+        colors-buffer (-> (BufferUtils/createFloatBuffer colors-count)
+                            (.put colors)
                             (.flip))
         ;; create & bind Vertex Buffer Object for vertices
         vbo-id (GL15/glGenBuffers)
         _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
         _ (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
+        _ (except-gl-errors "glBufferData 1")
+        cbo-id (GL15/glGenBuffers)
+        _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER cbo-id)
+        _ (GL15/glBufferData GL15/GL_ARRAY_BUFFER colors-buffer GL15/GL_STATIC_DRAW)
+        _ (except-gl-errors "glBufferData 2")
         ]
     (dosync (ref-set globals
                      (assoc @globals
                        :vbo-id vbo-id
-                       :vertices-count vertices-count)))))
+                       :vertices-count vertices-count
+                       :cbo-id cbo-id)))))
 
 (def vs-shader
   (str "#version 120\n"
        "\n"
-       "attribute vec4 in_Position;\n"
        "uniform float in_Angle;\n"
        "\n"
        "void main(void) {\n"
@@ -66,7 +113,8 @@
        "    mvp[1] = vec4(-sin(angle), cos(angle), 0.0, 0.0);\n"
        "    mvp[2] = vec4(0.0, 0.0, 1.0, 0.0);\n"
        "    mvp[3] = vec4(0.0, 0.0, 0.0, 1.0);\n"
-       "    gl_Position = mvp*in_Position;\n"
+       "    gl_Position = mvp*gl_Vertex;\n"
+       "    gl_FrontColor = gl_Color;\n"
        "}\n"
        ))
 
@@ -74,17 +122,19 @@
   (str "#version 120\n"
        "\n"
        "void main(void) {\n"
-       "    gl_FragColor = vec4(1.0, 1.0, 0.5, 1.0);\n"
+       ;;"    gl_FragColor = vec4(1.0, 1.0, 0.5, 1.0);\n"
+       "    gl_FragColor = gl_Color;\n"
        "}\n"
        ))
 
 (defn load-shader
   [shader-str shader-type]
   (let [shader-id (GL20/glCreateShader shader-type)
+        _ (except-gl-errors "glCreateShader")
         _ (GL20/glShaderSource shader-id shader-str)
-        ;;_ (println "init-shaders glShaderSource errors?" (GL11/glGetError))
+        _ (except-gl-errors "glShaderSource")
         _ (GL20/glCompileShader shader-id)
-        ;;_ (println "init-shaders glCompileShader errors?" (GL11/glGetError))
+        _ (except-gl-errors "glCompileShader")
         gl-compile-status (GL20/glGetShaderi shader-id GL20/GL_COMPILE_STATUS)
         ]
     (when (== gl-compile-status GL11/GL_FALSE)
@@ -97,15 +147,23 @@
   (let [vs-id (load-shader vs-shader GL20/GL_VERTEX_SHADER)
         fs-id (load-shader fs-shader GL20/GL_FRAGMENT_SHADER)
         p-id (GL20/glCreateProgram)
+        _ (except-gl-errors "glCreateProgram")
         _ (GL20/glAttachShader p-id vs-id)
+        _ (except-gl-errors "glAttachShader")
         _ (GL20/glAttachShader p-id fs-id)
+        _ (except-gl-errors "glAttachShader2")
+        ;;_ (GL20/glBindAttribLocation p-id 0 "in_Position")
+        ;;_ (except-gl-errors "glBindAttribLocation pos")
+        ;;_ (GL20/glBindAttribLocation p-id 1 "in_Color")
+        ;;_ (except-gl-errors "glBindAttribLocation col")
         _ (GL20/glLinkProgram p-id)
+        _ (except-gl-errors "glLinkProgram")
         gl-link-status (GL20/glGetProgrami p-id GL20/GL_LINK_STATUS)
         _ (when (== gl-link-status GL11/GL_FALSE)
             (println "ERROR: Linking Shaders:")
             (println (GL20/glGetProgramInfoLog p-id 10000)))
         angle-loc (GL20/glGetUniformLocation p-id "in_Angle")
-        ;;_ (println "init-shaders errors?" (GL11/glGetError))
+        _ (except-gl-errors "glGetUniformLocation")
         ]
     (dosync (ref-set globals
                      (assoc @globals
@@ -118,37 +176,53 @@
   []
   (let [{:keys [width height]} @globals]
     (println "OpenGL version:" (GL11/glGetString GL11/GL_VERSION))
-    (GL11/glClearColor 0.0 0.0 0.0 0.0)
+    (GL11/glClearColor 0.5 0.7 1.0 0.0)
     (GL11/glViewport 0 0 width height)
+    (GL11/glDisable GL11/GL_CULL_FACE)
     (init-buffers)
-    (init-shaders)
-    ;;(print "@globals")
-    ;;(pprint/pprint @globals)
-    ;;(println "")
-    ))
+    (init-shaders)))
 
 (defn draw
   []
   (let [{:keys [width height angle angle-loc
-                p-id vbo-id vertices-count]} @globals
+                p-id vbo-id vertices-count cbo-id]} @globals
                 w2 (/ width 2.0)
                 h2 (/ height 2.0)]
     (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
+    (except-gl-errors "glClear")
 
     (GL20/glUseProgram p-id)
-    ;; setup our uniform
+    (except-gl-errors "glUseProgram")
     (GL20/glUniform1f angle-loc angle)
+    (except-gl-errors "glUniform1f")
+
+    (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
+    (except-gl-errors "glBindBuffer v")
+    (GL11/glVertexPointer 3 GL11/GL_FLOAT 0 0)
+    (except-gl-errors "glVertexPointer")
+
+    (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER cbo-id)
+    (except-gl-errors "glBindBuffer c")
+    (GL11/glColorPointer 3 GL11/GL_FLOAT 0 0)
+    (except-gl-errors "glColorPointer")
 
     (GL11/glEnableClientState GL11/GL_VERTEX_ARRAY)
-    (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
-    (GL11/glVertexPointer 3 GL11/GL_FLOAT 0 0)
+    (except-gl-errors "glEnableClientState v")
+    (GL11/glEnableClientState GL11/GL_COLOR_ARRAY)
+    (except-gl-errors "glEnableClientState c")
+
     (GL11/glDrawArrays GL11/GL_TRIANGLES 0 vertices-count)
+    (except-gl-errors "glDrawArrays")
 
     ;; Put everything back to default (deselect)
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
+    (except-gl-errors "glBindBuffer 0")
     (GL11/glDisableClientState GL11/GL_VERTEX_ARRAY)
+    (except-gl-errors "glDisableClientState v")
+    (GL11/glDisableClientState GL11/GL_COLOR_ARRAY)
+    (except-gl-errors "glDisableClientState c")
     (GL20/glUseProgram 0)
-    ;;(println "draw errors?" (GL11/glGetError))
+    (except-gl-errors "glUseProgram 0")
     ))
 
 (defn update
