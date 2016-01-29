4910042fec287045b5a187eb5d6b5d824b50c257
Roger Allen <rallen@gmail.com>
2016-01-29 06:59:25 -0800

    fix jvm-opts to be per-os.  clean cruft
    Turns out the first thread option is only required on Mac OS X.

--- project.clj
+++ project.clj
@@ -1,25 +1,25 @@
-;; NOTE! This project requires leiningen 2.1.0 or later.
 (require 'leiningen.core.eval)
-;;(println (leiningen.core.eval/get-os)) ;; try lein deps to see this
 
-(def LWJGL-CLASSIFIER
-  "Per os native code classifier"
-  {:macosx "natives-osx"
-   :linux "natives-linux"
-   :windows "natives-windows"}) ;; TESTME
+;; per-os jvm-opts code cribbed from Overtone
+(def JVM-OPTS
+  {:common   []
+   :macosx   ["-XstartOnFirstThread"]
+   :linux    []
+   :windows  []})
 
-(defn lwjgl-classifier
-  "Return the os-dependent lwjgl native-code classifier"
-  []
-  (let [os (leiningen.core.eval/get-os)]
-    (get LWJGL-CLASSIFIER os)))
+(defn jvm-opts
+  "Return a complete vector of jvm-opts for the current os."
+  [] (let [os (leiningen.core.eval/get-os)]
+       (vec (set (concat (get JVM-OPTS :common)
+                         (get JVM-OPTS os))))))
 
 (defproject hello_lwjgl "0.3.0"
-  :description "Simple LWJGL clojure test."
+  :description "Simple LWJGL3 clojure test."
   :url "https://github.com/rogerallen/hello_lwjgl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [hello_lwjgl/lwjgl "3.0.0b1"]]
-  :jvm-opts ["-XstartOnFirstThread"]
+                 [hello_lwjgl/lwjgl   "3.0.0b1"]]
+  :min-lein-version "2.1.0"
+  :jvm-opts ^:replace ~(jvm-opts)
   :main hello-lwjgl.core)
