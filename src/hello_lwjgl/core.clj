8bf5e2d27f383ebc6469f93f6a2bb1fcda10bd18
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2022-09-23 05:28:13 -0700

    fix ugly (load-string) call

--- src/hello_lwjgl/core.clj
+++ src/hello_lwjgl/core.clj
@@ -6,28 +6,36 @@
             [hello-lwjgl.gamma :as gamma]
             ;;[hello-lwjgl.omega :as omega]
             )
-  (:import (org.lwjgl Version))
+  (:import [org.lwjgl Version])
   (:gen-class))
 
 
-;; ======================================================================
+
+;; ==================================================
+;; OSX glfw + repl compatability hack
+
 (defn start-cider-nrepl-in-new-thread
+  "Required on OSX -- glfw MUST be run from main thread, so this fn creates a
+  repl on a child thread for live code reloading.
+  Connect with cider by M-x cider-connect instead of M-x cider-jack-in"
   []
-  ;; for osx, you are required to run glfw from the main thread,
-  ;; thus we must run the repl from a child thread for live code reloading.
-  ;; You can connect to this child thread repl with cider by M-x cider-connect
-  ;; instead of the usual M-x cider-jack-in
-  (.start (Thread. (fn []
-                     (println "Starting Cider Nrepl Server Port 7888")
-                     (load-string (str ;; "(require '[clojure.tools.nrepl.server :as nrepl-server])"
-                                       "(require '[nrepl.server :as nrepl-server])"
-                                       "(require '[cider.nrepl :as cider])"
-                                       "(nrepl-server/start-server :port 7888 :handler cider/cider-nrepl-handler)"))))))
-
-;; ======================================================================
+  (.start
+   (Thread.
+    (fn []
+      (println "Starting Cider Nrepl Server Port 7888")
+      (eval `(do
+               (require 'nrepl.server)
+               (require 'cider.nrepl)
+               (nrepl.server/start-server
+                :port 7888
+                :handler cider.nrepl/cider-nrepl-handler)))))))
 
 
-;; added mutlimethod for easy future extensibility
+
+
+;; ==================================================
+;; Dispatch multimethod for easy future extensibility
+
 (defmulti main-dispatch identity)
 
 
@@ -50,6 +58,10 @@
   (throw (IllegalArgumentException. "Unknown Parameter")))
 
 
+
+
+;; ==================================================
+;; Main entry for program
 
 
 (defn -main
@@ -81,12 +93,15 @@
 
 (comment
 
+  ;; --- Live Code Reloading --- 
+
   ;; You have the ability to live reload code using the repl,
   ;; and thus dynamically change the render, while your app is running.
-  ;; It's pretty fun :).
+  ;; It's pretty neat :).
   
   ;; If you want to take advantage of live reloading, your opengl app must
   ;; run in a thread separate from the thread your repl is running in.
+  
   ;; Use an atom to communicate between threads by using reset! on the draw
   ;; function (or associated logic) within the repl thread, and dereference
   ;; the draw function in your main opengl loop.
@@ -98,5 +113,13 @@
   ;; to be able to play with live reloading
   (.start (Thread. (fn [] (alpha-live-reload/main))))
   ;; and try redefining the hot-draw atom within that namespace in your repl
+
+  ;; For OSX: lwjgl must be called from the main thread, so
+  ;; start this project with 'lein run alpha-live-reload cider' and connect
+  ;; to the repl in cider with M-x cider-connect.
+
+  ;; Reference
+  ;; https://stackoverflow.com/questions/38961679/how-to-inject-code-from-one-thread-to-another-in-clojure-for-live-opengl-editin
   
   )
+
